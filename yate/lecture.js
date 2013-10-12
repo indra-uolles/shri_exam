0f4ab6d298c59fde6a3a1da531b869081243ae47
indra-uolles <smirnovanatalia2008@gmail.com>
2013-10-13 00:26:27 +0400

    страница лекции на yate: фильтр по id лекции, обработка отсутствия данных, устранение эскейпинга

--- yate/lecture.js
+++ yate/lecture.js
@@ -19,26 +19,135 @@ var yr = yr || require('yate/lib/runtime.js');
 
     var M = new yr.Module();
 
-    var j0 = [ ];
+    // func display_photo() : xml
+    M.f0 = function f0(m, c0, i0, l0, a0) {
+        var r0 = '';
 
-    var j1 = [ 0, 'item' ];
+        function p0(m, c0, i0, l0) {
+            return 0;
+        }
 
-    var j2 = [ 0, 'name' ];
+        var j0 = [ 1, 0, 3, 0, 0, 'photo_url' ];
+
+        r0 += closeAttrs(a0);
+        if (nodeset2boolean( (m.s(j0, c0)) )) {
+            function p1(m, c0, i0, l0) {
+                return 0;
+            }
+
+            var j1 = [ 1, 0, 3, 0, 0, 'photo_url' ];
+
+            r0 += "<img src=\"" + nodeset2attrvalue( ( m.s(j1, c0) ) ) + "\" class=\"" + "people-photo box" + "\"/>";
+        } else {
+            r0 += "<img src=\"" + "./img/lectors/no_photo.png" + "\" class=\"" + "people-photo box" + "\"/>";
+        }
+
+        return r0;
+    };
+
+    // func display_presentation() : xml
+    M.f1 = function f1(m, c0, i0, l0, a0) {
+        var r0 = '';
+
+        function p2(m, c0, i0, l0) {
+            return 0;
+        }
+
+        var j2 = [ 1, 0, 3, 0, 0, 'slides_url' ];
+
+        r0 += closeAttrs(a0);
+        if (nodeset2boolean( (m.s(j2, c0)) )) {
+            function p3(m, c0, i0, l0) {
+                return 0;
+            }
+
+            var j3 = [ 1, 0, 3, 0, 0, 'slides_url' ];
+
+            r0 += "<iframe src=\"" + nodeset2attrvalue( ( m.s(j3, c0) ) ) + "\" width=\"" + "427" + "\" height=\"" + "356" + "\" frameborder=\"" + "0" + "\" marginwidth=\"" + "0" + "\" marginheight=\"" + "0" + "\" scrolling=\"" + "no" + "\" allowfullscreen=\"" + "true" + "\"></iframe>";
+        } else {
+            r0 += "Увы, но мы еще не загрузили презентацию. Скоро загрузим!";
+        }
+
+        return r0;
+    };
+
+    // func display_video() : xml
+    M.f2 = function f2(m, c0, i0, l0, a0) {
+        var r0 = '';
+
+        function p4(m, c0, i0, l0) {
+            return 0;
+        }
+
+        var j4 = [ 1, 0, 3, 0, 0, 'video_url' ];
+
+        r0 += closeAttrs(a0);
+        if (nodeset2boolean( (m.s(j4, c0)) )) {
+            function p5(m, c0, i0, l0) {
+                return 0;
+            }
+
+            var j5 = [ 1, 0, 3, 0, 0, 'video_url' ];
+
+            r0 += "<object width=\"" + "450" + "\" height=\"" + "361" + "\"><param name=\"" + "src" + "\" value=\"" + nodeset2attrvalue( ( m.s(j5, c0) ) ) + "\"/><param name=\"" + "allowFullScreen" + "\" value=\"" + "true" + "\"/><param name=\"" + "scale" + "\" value=\"" + "noscale" + "\"/><embed src=\"" + nodeset2attrvalue( ( m.s(j5, c0) ) ) + "\" type=\"" + "application/x-shockwave-flash" + "\" width=\"" + "450" + "\" height=\"" + "361" + "\" allowFullScreen=\"" + "true" + "\" scale=\"" + "noscale" + "\"/>" + " " + "</object>";
+        } else {
+            r0 += "Увы, но мы еще не загрузили видео. Скоро загрузим!";
+        }
+
+        return r0;
+    };
+
+    // func display_element() : xml
+    M.f3 = function f3(m, c0, i0, l0, a0) {
+        var r0 = '';
+
+        function p6(m, c0, i0, l0) {
+            return 0;
+        }
+
+        var j6 = [ 1, 0, 3, 0, 0, 'name' ];
+
+        var j7 = [ 1, 0, 3, 0, 0, 'teacher_name' ];
+
+        r0 += closeAttrs(a0);
+        r0 += "<p>";
+        r0 += "<a href=\"" + "#" + "\">" + "Программа(все лекции) &gt; " + "</a>";
+        r0 += nodeset2xml( ( m.s(j6, c0) ) );
+        r0 += "</p>";
+        r0 += "<h3>" + nodeset2xml( ( m.s(j6, c0) ) ) + "</h3>";
+        r0 += "<div class=\"" + "teacher-container" + "\">";
+        r0 += "<a class=\"" + "people-sticker-inner" + "\" href=\"" + "#" + "\">";
+        r0 += m.f('f0', c0, i0, l0, a0);
+        r0 += "<p class=\"" + "yandex" + "\">" + nodeset2xml( ( m.s(j7, c0) ) ) + "</p></a>";
+        r0 += "</div>";
+        r0 += "<div>";
+        r0 += "<div class=\"" + "horiz-menu" + "\">";
+        r0 += "<div class=\"" + "horiz-menu-item" + "\">";
+        r0 += "<a id=\"" + "show_hide_present" + "\" href=\"" + "#" + "\">" + "Посмотреть презентацию" + "</a>";
+        r0 += "</div>";
+        r0 += "<div class=\"" + "horiz-menu-item" + "\">";
+        r0 += "<a id=\"" + "show_hide_video" + "\" href=\"" + "#" + "\">" + "Посмотреть видео" + "</a>";
+        r0 += "</div>";
+        r0 += "</div>";
+        r0 += "<div class=\"" + "hidden" + "\" id=\"" + "presentation" + "\">";
+        r0 += m.f('f1', c0, i0, l0, a0);
+        r0 += "</div>";
+        r0 += "<div class=\"" + "hidden" + "\" id=\"" + "video" + "\">";
+        r0 += m.f('f2', c0, i0, l0, a0);
+        r0 += "</div>";
+        r0 += "</div>";
+
+        return r0;
+    };
+
+    var j8 = [ ];
 
     // match /
     M.t0 = function t0(m, c0, i0, l0, a0) {
         var r0 = '';
 
         r0 += closeAttrs(a0);
-        var items0 = selectNametest('item', c0, []);
-        for (var i1 = 0, l1 = items0.length; i1 < l1; i1++) {
-            var c1 = items0[ i1 ];
-            if ((nodeset2scalar( ( selectNametest('name', c1, []) ) ) == "HTTP протокол")) {
-                r0 += "yes";
-            } else {
-                r0 += "no";
-            }
-        }
+        r0 += m.f('f3', c0, i0, l0, a0);
 
         return r0;
     };
