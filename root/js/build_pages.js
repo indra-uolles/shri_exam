aae69935c09baffa3447266f5f0b18d95e0d693c
SMIRNOVA NATALIA <smirnovanatalia2008@gmail.com>
2013-10-17 10:34:53 +0400

    немного причесала уровни абстракций в SPA.html

---
+++ root/js/build_pages.js
@@ -0,0 +1,78 @@
+function get_logo(){ 
+    var data = {
+        logo_name: 'Яндекс ШРИ в Москве 2013',
+        when_where: '7 сентября - 31 декабря'
+    };
+    var cpl_logo = yr.run('logo', data);
+    return cpl_logo;
+}
+
+function get_horiz_menu(){   
+    data = {
+        item: [{
+            link: '#',
+            title: 'О мероприятии',
+            id: 'page_index'
+        }, {
+            link: '#',
+            title: 'Программа',
+            id: 'page_lectures'
+        }, {
+            link: '#',
+            title: 'Докладчики',
+            id: 'page_lectors'
+        }, {
+            link: '#',
+            title: 'Правила отбора',
+            id: 'page_rules'
+        }, {
+            link: '#',
+            title: 'FAQ',
+            id: 'page_faq'
+        }, {
+            link: '#',
+            title: 'Выпускники',
+            id: 'page_students'
+        }]
+    };
+    var cpl_menu = yr.run('horizontal_menu', data);
+    return cpl_menu;
+}
+
+function get_lectors_filter(){
+    var data = {
+        item: [{
+            link: '#',
+            id: '1',
+            class: 'lectors_filter',
+            title: 'Все'
+        }, {
+            link: '#',
+            id: '2',
+            class: 'lectors_filter',
+            title: 'Инструменты'
+        }, {
+            link: '#',
+            id: '3',
+            class: 'lectors_filter',
+            title: 'Технологии'
+        }, {
+            link: '#',
+            id: '4',
+            class: 'lectors_filter',
+            title: 'Языки'
+        }, {
+            link: '#',
+            id: '5',
+            class: 'lectors_filter',
+            title: 'Дизайн'
+        }, {
+            link: '#',
+            id: '6',
+            class: 'lectors_filter',
+            title: 'Фреймворки'
+        }]
+    }
+    var cpl_content_filter = yr.run('lectors_filter', data);
+    return cpl_content_filter;
+}
